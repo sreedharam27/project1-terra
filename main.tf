4bfdf731b6fc2bd10e492e94c6e4f7bee9c1b35d
sreedharam27 <119181206+sreedharam27@users.noreply.github.com>
2023-03-04 18:24:54 +0530

    bscpro-3

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 Provider  "aws" {
-	region = "us-east-1"
-	access_key = "AKIASTZS7WGYRCG35O6A"
-	secret_key = "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"
-	}
+region = "us-east-1"
+access_key = "AKIASTZS7WGYRCG35O6A"
+secret_key = "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"
+}
 resource "aws_instance" "one" {
   ami               ="ami-006dcf34c09e50022"
   instance_type     = "t2.micro"

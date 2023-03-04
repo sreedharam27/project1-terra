c2a4cb8b208bd05ec7c9b44d6fe8de9a8d00f6ad
sreedharam27 <119181206+sreedharam27@users.noreply.github.com>
2023-03-04 18:08:00 +0530

    pro-smplcmt 1

---
+++ main.tf
@@ -0,0 +1,15 @@
+Provider"aws" {
+	region = "us-east-1"
+	access_key = "AKIASTZS7WGYRCG35O6A"
+	secret_key = "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"
+	}
+resource "aws_instance" "one" {
+  ami               ="ami-006dcf34c09e50022"
+  instance_type     = "t2.micro"
+  keyname           = "freshpair"
+  availability_zone = "us-east-1a"
+  tags = {
+  Name = "basic project"
+  }
+}
+    

6d0e3b3f0d4a58d6a11cce9202b0cd566cc46552
sreedharam27 <119181206+sreedharam27@users.noreply.github.com>
2023-03-05 09:33:35 +0530

    advcmt-2

--- main.tf
+++ main.tf
@@ -3,3 +3,41 @@ region = "us-east-1"
 access_key = "AKIASTZS7WGYUAILQZIZ"
 secret_key = "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"
 }
+
+resource "aws_instance" "one" {
+  ami               ="ami-006dcf34c09e50022"
+  instance_type     = "t2.micro"
+  key_name          = "freshpair"
+  security_group    = ["elb-sg"]
+  availability_zone = "us-east-1b"
+  user_data = <<-EOF
+  #!/bin/bash
+  sudo -i
+  yum install httpd -y
+  systemctl start httpd -y
+  chkconfig httpd on
+  echo "hello everyone this my new instance server-1" > /var/lib/www/index.html
+  EOF
+  tags = {
+  Name = "server-1"
+  }
+}
+    
+resource "aws_instance" "two" {
+  ami               ="ami-006dcf34c09e50022"
+  instance_type     = "t2.micro"
+  key_name          = "freshpair"
+  security_group    = ["elb-sg"]
+  availability_zone = "us-east-1c"
+  user_data = <<-EOF
+  #!/bin/bash
+  sudo -i
+  yum install httpd -y
+  systemctl start httpd -y
+  chkconfig httpd on
+  echo "hello everyone this my new instance server-2" > /var/lib/www/index.html
+  EOF
+  tags = {
+  Name = "server-2"
+  }
+}

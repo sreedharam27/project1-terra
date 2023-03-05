6341d3a844005cfaf0a65755c387dd83a9a87d86
sreedharam27 <119181206+sreedharam27@users.noreply.github.com>
2023-03-05 09:45:51 +0530

    advcmt-4

--- main.tf
+++ main.tf
@@ -5,10 +5,10 @@ secret_key = "txcSe6uvg9HUs/Rg6Skxc6jwXbb1zET0Z3IdG9VU"
 }
 
 resource "aws_instance" "one" {
-  ami               ="ami-006dcf34c09e50022"
-  instance_type     = "t2.micro"
-  key_name          = "freshpair"
-  security_group    = sg.aws_security_group_elb-sg.id
+  ami                       ="ami-006dcf34c09e50022"
+  instance_type             = "t2.micro"
+  key_name                  = "freshpair"
+  vpc_security_group_ids    = [sg.aws_security_group_elb-sg.id]
   availability_zone = "us-east-1b"
   user_data = <<-EOF
   #!/bin/bash
@@ -24,11 +24,11 @@ resource "aws_instance" "one" {
 }
     
 resource "aws_instance" "two" {
-  ami               ="ami-006dcf34c09e50022"
-  instance_type     = "t2.micro"
-  key_name          = "freshpair"
-  security_group    = sg.aws_security_group_elb-sg.id
-  availability_zone = "us-east-1c"
+  ami                       ="ami-006dcf34c09e50022"
+  instance_type             = "t2.micro"
+  key_name                  = "freshpair"
+  vpc_security_group_ids    = [sg.aws_security_group_elb-sg.id]
+  availability_zone         = "us-east-1c"
   user_data = <<-EOF
   #!/bin/bash
   sudo -i

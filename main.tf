92e200e242884ee21cfbba7c7ed7027597a52c15
sreedharam27 <119181206+sreedharam27@users.noreply.github.com>
2023-03-05 09:56:45 +0530

    advcmt-2

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "one" {
   ami                       ="ami-006dcf34c09e50022"
   instance_type             = "t2.micro"
   key_name                  = "freshpair"
-  vpc_security_group_ids    = [sg.aws_security_group_elb-sg.id]
+  vpc_security_group_ids    = [aws_security_group_elb-sg.id]
   availability_zone = "us-east-1b"
   user_data = <<-EOF
   #!/bin/bash
@@ -27,7 +27,7 @@ resource "aws_instance" "two" {
   ami                       ="ami-006dcf34c09e50022"
   instance_type             = "t2.micro"
   key_name                  = "freshpair"
-  vpc_security_group_ids    = [sg.aws_security_group_elb-sg.id]
+  vpc_security_group_ids    = [aws_security_group_elb-sg.id]
   availability_zone         = "us-east-1c"
   user_data = <<-EOF
   #!/bin/bash
@@ -39,5 +39,36 @@ resource "aws_instance" "two" {
   EOF
   tags = {
   Name = "server-2"
+  }
+}
+
+resource "aws_security_group" "elb-sg" {
+  name        = "elb-sg"
+  
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+   ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  tags = {
+    Name = "elb-sg"
   }
 }

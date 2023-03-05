96283215cb144326c2ee2c99af68b4610679a6a0
sreedharam27 <119181206+sreedharam27@users.noreply.github.com>
2023-03-05 15:44:54 +0530

    adv-cmt-2

--- main.tf
+++ main.tf
@@ -46,16 +46,16 @@ resource "aws_security_group" "three" {
   name        = "elb-sg"
   
   ingress {
-    from_port        = 22
-    to_port          = 22
+    from_port        = 80
+    to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     
   }
 
    ingress {
-    from_port        = 80
-    to_port          = 80
+    from_port        = 22
+    to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     
